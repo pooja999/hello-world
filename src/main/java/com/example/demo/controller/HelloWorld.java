d8096487425f473fbef641636080997e50bb31ab
Harshith Patte <harshithpatte@gmail.com>
2024-12-13 11:48:40 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/helloworld")
+    public String sayHelloWorld() {
+        return "hello world!";
+    }
+}
