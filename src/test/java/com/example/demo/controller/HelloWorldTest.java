5f330fe5b84cf985f371eeb086ed30b70a6f64a4
Harshith Patte <harshithpatte@gmail.com>
2024-12-13 17:10:45 +0530

    add tests

---
+++ src/test/java/com/example/demo/controller/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class HelloWorldTest {
+
+    @InjectMocks
+    HelloWorld helloWorld;
+    @Test
+    void sayHelloWorld() {
+        assertEquals("hello world!", helloWorld.sayHelloWorld());
+    }
+}
